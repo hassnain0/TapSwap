1a42616444398f210599c00318d96b406c850ff8
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-09-16 23:43:38 +0500

    Web Link Added

--- bot/index.js
+++ bot/index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://7655-121-52-154-154.ngrok-free.app";
+const web_link = "https://funny-sopapillas-5682e4.netlify.app/";
 const community_link = "https://t.me/rockipointannouncement";
 
 
