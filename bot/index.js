c62a2ab80f95a4cadfce8078093e9e71f0f0744e
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-09-21 14:59:39 +0500

    Url

--- bot/index.js
+++ bot/index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://resilient-kitsune-969152.netlify.app/";
+const web_link = "https://silver-boba-ecff9d.netlify.app/";
 const community_link = "https://t.me/rockipoint";
 
 
