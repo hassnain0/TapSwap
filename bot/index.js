34fb392ee91e3ea38d5f65bf831c69af9d3f53ae
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-09-28 22:21:49 +0500

    Tokem

--- bot/index.js
+++ bot/index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://thriving-boba-ee98d9.netlify.app/";
+const web_link = "https://relaxed-beignet-4afed7.netlify.app/";
 const community_link = "https://t.me/Kryptoblockchain";
 
 
--- mini-app/src/firebase.js
+++ mini-app/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey:"AIzaSyCRH4HOEDo9M2emvyLXIDrmkJQ_39Qekuo",
+    authDomain: "tapswap-e9c29.firebaseapp.com",
+    projectId:" tapswap-e9c29",
+    storageBucket: "tapswap-e9c29.appspot.com",
+    messagingSenderId: "802210819831",
+    appId: "1:802210819831:web:e3ba2057cdf55d36cbedf2",
+    measurementId:"G-W9MH7ZQK1N",
 
 };
 
